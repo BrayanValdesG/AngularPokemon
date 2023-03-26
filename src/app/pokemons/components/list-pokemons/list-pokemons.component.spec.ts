5fe1dfc49d01010036b676bf5af24085c1e78b0b
BrayanValdesG <balexis41@gmail.com>
2023-03-26 18:37:48 -0500

    intento de spec

--- src/app/pokemons/components/list-pokemons/list-pokemons.component.spec.ts
+++ src/app/pokemons/components/list-pokemons/list-pokemons.component.spec.ts
@@ -1,23 +1,75 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListPokemonsComponent } from './list-pokemons.component';
+import {ChangeDetectionStrategy, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PokemonService } from '../../services/pokemon.service';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchPokemonPipe } from '@pokemons/pipes/search-pokemon.pipe';
+import { SharedModule } from '@shared/shared.module';
+import { NgOptimizedImage } from '@angular/common';
 
 describe('ListPokemonsComponent', () => {
-  let component: ListPokemonsComponent;
-  let fixture: ComponentFixture<ListPokemonsComponent>;
+  let inputSearch: any;
+
+  const getByTestId = (id: any, compiled: any) => {
+    return compiled.querySelector(`[data-test-id="${id}"]`);
+  };
+
+  let pokemonServiceStub: Partial<PokemonService>;
 
   beforeEach(async () => {
+    pokemonServiceStub = {};
     await TestBed.configureTestingModule({
-      declarations: [ ListPokemonsComponent ]
+      declarations: [ ListPokemonsComponent, SearchPokemonPipe  ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        SharedModule,
+        NgOptimizedImage
+      ],
+      providers: [
+        { provide: PokemonService, useValue: pokemonServiceStub}
+      ],
+      schemas : [CUSTOM_ELEMENTS_SCHEMA]
+    })
+    .overrideComponent(ListPokemonsComponent, {
+      set: {changeDetection: ChangeDetectionStrategy.Default}
     })
     .compileComponents();
-
-    fixture = TestBed.createComponent(ListPokemonsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+
+  const factory = () => {
+    const fixture: ComponentFixture<ListPokemonsComponent> = TestBed.createComponent(ListPokemonsComponent);
+    const component: ListPokemonsComponent = fixture.componentInstance;
+    const compiled = fixture.debugElement.nativeElement;
+    // const pokemonService = TestBed.inject(PokemonService);
+
+    inputSearch = getByTestId('inputSearch', compiled);
+    fixture.detectChanges();
+    return {
+      fixture, component, compiled
+    };
+  };
+
+  it('Should render the initial UI as expected', () => {
+    const {component, compiled} = factory();
     expect(component).toBeTruthy();
+    expect(inputSearch.value.trim()).toBeFalsy();
+      const tBody = getByTestId('records-body', compiled);
+      expect(tBody.children.length).toEqual(4);
+      Array.from(tBody.children).forEach((node: HTMLElement, i) => {
+        const tds = Array.from(node.querySelectorAll('td'));
+        if (i === 0) {
+          expect(tds[0].innerHTML).toEqual('Dittoo');
+        } else if (i === 2) {
+          expect(tds[0].innerHTML).toEqual('Eevee');
+        } else if(i === 3) {
+          expect(tds[0].innerHTML).toEqual('Alakazam');
+        }
+      });
+
   });
 });
